002d24f54395e169e7c0c01e1845e98d0a129e3a
scottgilmartin <41211277+scottgilmartin@users.noreply.github.com>
2019-02-03 22:45:08 +0000

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,33 +1,35 @@
-def split_lyrics(lyric,entries):
+def split_lyrics(lyric, entries):
     """
     Use nltk to split the lyrics into arpabet phones
     """
-    line_arpas=[]
-    arpa_words=[]
+    line_arpas = []
+    arpa_words = []
 
     for n in lyric:
-        l=[]
+        l = []
         for subent in entries:
             if n.lower() in subent:
                 l.append(subent)
                 break
-            elif n.lower()=='rtrn':
-                l.append(('rtrn',['R1']))
-                
+            elif n.lower() == 'rtrn':
+                l.append(('rtrn', ['R1']))
+
         for arp in l[0][1]:
             line_arpas.append(arp)
             arpa_words.append(l[0][0])
     return line_arpas, arpa_words
 
+
 def contain_nums(s):
     return any(i.isdigit() for i in s)
 
-def generate_pairs(chunks): 
+
+def generate_pairs(chunks):
     """
     Generate all possible permutations of length 2 (order doesn't matter)
     """
-    outcomes=chunks
-    length=2
+    outcomes = chunks
+    length = 2
     ans = set([()])
     for dummy_idx in range(length):
         temp = set()
